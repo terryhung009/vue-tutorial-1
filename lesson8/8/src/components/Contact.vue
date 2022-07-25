e8a7ff6541ab1061ba2eef424679c5d1bc422ab5
denny <khc741213@gmail.com>
2022-07-25 10:58:22 +0800

    1

---
+++ lesson8/8/src/components/Contact.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Contact Us</h1>
+
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+<style>
+
+
+</style>
---
+++ lesson8/8/src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Account from '@/components/Account.vue'
+import Contact from '@/components/Contact.vue'
+import Friends from '@/components/Friends.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/friends',
+    name: 'Friends',
+    component: Friends
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact
+  },
+  {
+    path: '/account',
+    name: 'Account',
+    component: Account
+  },
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
